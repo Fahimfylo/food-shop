39c11eb10fd8d8f0b6501c3b6c7e7898b98dc17c
ghost <ghostpy91.com>
2025-03-26 03:19:52 -0700

    cors changes

--- server/index.js
+++ server/index.js
@@ -321,9 +321,12 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("boss is running");
 });
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express on Vercel!");
 });
+
+module.exports = app;
 
 /**
  * ----------------------------
